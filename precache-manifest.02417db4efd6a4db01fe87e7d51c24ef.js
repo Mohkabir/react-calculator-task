7bb3ef48b1626e857cb99c9aedbf3051676ae190
mohammed kabir <kbshowkb@gmail.com>
2020-12-13 07:53:19 -1200

    Updates

---
+++ precache-manifest.02417db4efd6a4db01fe87e7d51c24ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4ef3730eb5e1a1841cf510030c247d",
+    "url": "/react-calculator-task/index.html"
+  },
+  {
+    "revision": "c5fd585bbbbb2b20c369",
+    "url": "/react-calculator-task/static/css/main.127e749b.chunk.css"
+  },
+  {
+    "revision": "3539412a4aaabbb0421a",
+    "url": "/react-calculator-task/static/js/2.256d4939.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-calculator-task/static/js/2.256d4939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fd585bbbbb2b20c369",
+    "url": "/react-calculator-task/static/js/main.c5f00bf5.chunk.js"
+  },
+  {
+    "revision": "d1fcad5f58f7e88486b5",
+    "url": "/react-calculator-task/static/js/runtime-main.84d8efe3.js"
+  }
+]);
